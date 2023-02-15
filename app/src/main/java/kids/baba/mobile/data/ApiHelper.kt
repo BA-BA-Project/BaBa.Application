e269e3d88e85908998f4b6a8fdbe2c2a35ff6943
Hong Sunghwi <tnfy10@gmail.com>
2023-02-15 22:57:49 +0900

    retrofit 생성 코드 추가

---
+++ app/src/main/java/kids/baba/mobile/data/ApiHelper.kt
@@ -0,0 +1,30 @@
+package kids.baba.mobile.data
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiHelper {
+    private val gson = GsonBuilder().setLenient().create()
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        builder.apply {
+            addInterceptor(loggingInterceptor)
+        }
+        return builder.build()
+    }
+
+    fun <T> create(
+        service: Class<T>,
+        baseUrl: String
+    ): T = Retrofit.Builder().apply {
+        baseUrl(baseUrl)
+        addConverterFactory(GsonConverterFactory.create(gson))
+        client(createOkHttpClient())
+    }.build().create(service)
+}
