53b3789b54696f0c9bbff486e699cf45c7e20224
TRASALBY <ghtjd8264@naver.com>
2023-02-22 15:18:04 +0900

    상태에 따른 화면 이동

--- app/src/main/java/kids/baba/mobile/presentation/viewmodel/IntroViewModel.kt
+++ app/src/main/java/kids/baba/mobile/presentation/viewmodel/IntroViewModel.kt
@@ -3,38 +3,48 @@ package kids.baba.mobile.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kids.baba.mobile.core.constant.PrefsKey
+import kids.baba.mobile.core.utils.EncryptedPrefs
+import kids.baba.mobile.domain.usecase.GetMemberUseCase
 import kids.baba.mobile.presentation.state.IntroUiState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class IntroViewModel @Inject constructor(
-
-) : ViewModel(){
+    private val getMemberUseCase: GetMemberUseCase
+) : ViewModel() {
     private var _uiState = MutableStateFlow<IntroUiState>(IntroUiState.Loading)
     val uiState = _uiState.asStateFlow()
+
+    private var isLoggedIn = false
 
     init {
         checkLogin()
     }
 
-    private fun tempUseCase() : Result<Boolean>{
-        return kotlin.runCatching { true }
-    }
-
     private fun checkLogin() {
         viewModelScope.launch {
             delay(SPLASH_TIME)
-            val isLoggedIn = tempUseCase().getOrThrow() //로그인 확인 usecase 적용
-            if(isLoggedIn) {
-                val isOnboarded = tempUseCase().getOrThrow() // 온보드 확인 usecase 적용
-                _uiState.value =
-                    if(false) IntroUiState.AlreadyLoggedIn else IntroUiState.NeedToOnboard
+            getMemberUseCase.getMe().catch {
+                isLoggedIn = false
+            }.collect {
+                isLoggedIn = true
+            }
+
+            if (isLoggedIn) {
+                val isOnboarded = EncryptedPrefs.getBoolean(PrefsKey.ON_BOARDING_KEY)
+                _uiState.value = if(isOnboarded){
+                    IntroUiState.AlreadyLoggedIn
+                } else {
+                    IntroUiState.NeedToOnboard
+                }
             } else {
-                _uiState.value = IntroUiState.NeedToLogin
+                _uiState.value = IntroUiState.NeedToOnboard
             }
         }
     }
