3bace5ab767c705cf572992fb08f85ae04502815
Hong Sunghwi <tnfy10@gmail.com>
2023-02-26 16:31:29 +0900

    카카오 sdk & proguard 설정
    - 테스트용으로 카카오 공식 문서에 있는 로그인 예제 추가

--- app/src/main/java/kids/baba/mobile/presentation/view/MainActivity.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/MainActivity.kt
@@ -3,15 +3,71 @@ package kids.baba.mobile.presentation.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
-import kids.baba.mobile.R
+import kids.baba.mobile.BuildConfig
+import kids.baba.mobile.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        kakaoLoginExample() // 카카오 로그인 예제
+    }
+
+    private fun kakaoLoginExample() {
+        Log.e(javaClass.simpleName, "디버그 키해시: ${Utility.getKeyHash(this)}")
+
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+
+        binding.kakaoLoginTestBtn.setOnClickListener {
+            // 로그인 조합 예제
+
+            // 카카오계정으로 로그인 공통 callback 구성
+            // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e(javaClass.simpleName, "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Log.i(javaClass.simpleName, "카카오계정으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e(javaClass.simpleName, "카카오톡으로 로그인 실패", error)
+
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i(javaClass.simpleName, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
     }
 
     companion object {
