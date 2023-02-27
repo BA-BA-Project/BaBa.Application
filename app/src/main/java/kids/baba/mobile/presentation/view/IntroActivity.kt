052f44d1348e1eee30bdfbf93a5dad5c63fa2945
TRASALBY <ghtjd8264@naver.com>
2023-02-27 22:17:30 +0900

    jetPack 네비게이션 적용

--- app/src/main/java/kids/baba/mobile/presentation/view/IntroActivity.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/IntroActivity.kt
@@ -4,38 +4,38 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kids.baba.mobile.R
 import kids.baba.mobile.presentation.extension.repeatOnStarted
 import kids.baba.mobile.presentation.state.IntroUiState
-import kids.baba.mobile.presentation.view.onboarding.OnBoardingFragment
 import kids.baba.mobile.presentation.viewmodel.IntroViewModel
 
 @AndroidEntryPoint
 class IntroActivity : AppCompatActivity() {
 
     private val viewModel: IntroViewModel by viewModels()
-    private val onBoardingFragment by lazy {
-        OnBoardingFragment()
-    }
 
-    private val loginFragment by lazy {
-        LoginFragment()
-    }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-
+        setNavController()
         collectUiState()
+    }
+
+    private fun setNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_intro) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
     private fun collectUiState() {
         repeatOnStarted {
             viewModel.uiState.collect { uiState ->
-                val transaction = supportFragmentManager.beginTransaction()
                 when (uiState) {
                     IntroUiState.AlreadyLoggedIn -> {
                         MainActivity.startActivity(this)
@@ -43,15 +43,10 @@ class IntroActivity : AppCompatActivity() {
                     }
 
                     IntroUiState.NeedToLogin -> {
-                        transaction.replace(R.id.fcv_intro, loginFragment).commit()
+                        navController.navigate(R.id.action_onBoardingFragment_to_loginFragment)
                     }
 
-                    IntroUiState.NeedToOnboard -> {
-                        transaction.replace(R.id.fcv_intro, onBoardingFragment).commit()
-                    }
-
-                    else -> {
-                    }
+                    else -> Unit
                 }
             }
         }
