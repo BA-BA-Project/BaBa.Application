2569314836c1e2e742b525d4bc36b2c2307f9e6d
TRASALBY <ghtjd8264@naver.com>
2023-03-08 14:23:31 +0900

    프로필 아이콘 리소스 추가

---
+++ app/src/main/java/kids/baba/mobile/presentation/view/signup/ProfileIcon.kt
@@ -0,0 +1,9 @@
+package kids.baba.mobile.presentation.view.signup
+
+import androidx.annotation.DrawableRes
+
+data class ProfileIcon(
+    @DrawableRes
+    val icon: Int,
+    val isSelected: Boolean
+)
