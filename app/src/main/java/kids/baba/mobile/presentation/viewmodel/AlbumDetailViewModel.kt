820c2d6ab13359b2d5425bef2c499212b81c3557
TRASALBY <ghtjd8264@naver.com>
2023-03-21 16:55:41 +0900

    Dialog 기본 구성 및 dataBinding

---
+++ app/src/main/java/kids/baba/mobile/presentation/viewmodel/AlbumDetailViewModel.kt
@@ -0,0 +1,10 @@
+package kids.baba.mobile.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AlbumDetailViewModel @Inject constructor(
+)  : ViewModel(){
+}
