4c111df06200cf95a3039d20b834f4b45a64847a
TRASALBY <ghtjd8264@naver.com>
2023-03-22 14:50:48 +0900

    Glide를 사용해 링크로 이미지 출력

--- app/src/main/java/kids/baba/mobile/presentation/binding/BindingAdpater.kt
+++ app/src/main/java/kids/baba/mobile/presentation/binding/BindingAdpater.kt
@@ -3,9 +3,17 @@ package kids.baba.mobile.presentation.binding
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 
 @BindingAdapter("iconRes")
 fun setIcon(imageView: ImageView, @DrawableRes res: Int){
     imageView.setImageResource(res)
+}
+
+@BindingAdapter("imageFromUrl")
+fun setImageFromUrl(imageView: ImageView, url: String){
+    Glide.with(imageView.context)
+        .load(url)
+        .into(imageView)
 }
