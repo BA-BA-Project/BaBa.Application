1f6a31ffe0bff7e910bfdd87be6b234f5591696a
TRASALBY <ghtjd8264@naver.com>
2023-03-27 21:09:55 +0900

    tempMember 지정

--- app/src/main/java/kids/baba/mobile/domain/usecase/GetMemberUseCase.kt
+++ app/src/main/java/kids/baba/mobile/domain/usecase/GetMemberUseCase.kt
@@ -1,9 +1,7 @@
 package kids.baba.mobile.domain.usecase
 
-import android.util.Log
 import kids.baba.mobile.core.constant.PrefsKey
 import kids.baba.mobile.core.error.MemberNotFoundException
-import kids.baba.mobile.core.error.TokenEmptyException
 import kids.baba.mobile.core.utils.EncryptedPrefs
 import kids.baba.mobile.domain.repository.MemberRepository
 import kids.baba.mobile.presentation.mapper.toPresentation
@@ -17,7 +15,11 @@ import javax.inject.Inject
 
 class GetMemberUseCase @Inject constructor(private val memberRepository: MemberRepository) {
     private val tag = "GetMemberUseCase"
-
+    private val tempMember = MemberUiModel(
+        "이호성",
+        "안녕하세요",
+        UserIconUiModel(UserProfileIconUiModel.PROFILE_G_1,"#FFA500"),
+    )
     fun getMe() = flow {
         emit(EncryptedPrefs.getMember(PrefsKey.MEMBER_KEY).toPresentation())
     }.catch {
@@ -25,19 +27,18 @@ class GetMemberUseCase @Inject constructor(private val memberRepository: MemberR
             val accessToken = EncryptedPrefs.getString(PrefsKey.ACCESS_TOKEN_KEY)
 
             if (accessToken.isEmpty()) {
-                val msg = "accessToken이 존재하지 않음."
-                Log.e(tag, msg)
-                throw TokenEmptyException(msg)
+//           로그인 과정이 없어 에러가 발생하므로 잠시 주석 이후 합치는 과정에서 원복 해야함
+//                val msg = "accessToken이 존재하지 않음."
+//                Log.e(tag, msg)
+//                throw TokenEmptyException(msg)
+                emit(tempMember)
+            }else {
+                val member = memberRepository.getMe(accessToken).first()
+                EncryptedPrefs.putMember(PrefsKey.MEMBER_KEY, member)
+                emit(member.toPresentation())
             }
-            val member = memberRepository.getMe(accessToken).first()
-            EncryptedPrefs.putMember(PrefsKey.MEMBER_KEY, member)
-            emit(member.toPresentation())
+
         } else {
-            val tempMember = MemberUiModel(
-                "이호성",
-                "안녕하세요",
-                UserIconUiModel(UserProfileIconUiModel.PROFILE_G_1,"#ff1234"),
-            )
             emit(tempMember)
 //            throw it
         }
