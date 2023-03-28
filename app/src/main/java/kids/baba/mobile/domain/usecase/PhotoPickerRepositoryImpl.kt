5d4aa3880a813491923093c07412e415fd75f7ad
심지훈 <simjihun@simjihuns-MacBook-Air.local>
2023-03-28 15:17:26 +0900

    Fix the photo-picker problem caused by ExtensionVersion R 2

--- app/src/main/java/kids/baba/mobile/domain/usecase/PhotoPickerRepositoryImpl.kt
+++ app/src/main/java/kids/baba/mobile/domain/usecase/PhotoPickerRepositoryImpl.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.res.Resources
 import android.net.Uri
 import android.os.Build
+import android.os.ext.SdkExtensions.getExtensionVersion
 import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Log
+import androidx.annotation.RequiresApi
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kids.baba.mobile.R
 import kids.baba.mobile.domain.model.MediaData
@@ -23,12 +25,13 @@ class PhotoPickerRepositoryImpl @Inject constructor(
 
     private val TAG = "GetPhotoFromPickerUseCase"
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override suspend fun getPhoto(uri: Uri) = flow {
 
         val date = getRealPathFromUri(uri)
 
         val mediaData = MediaData(
-            mediaName = /*uri.name*/uri.toString(),
+            mediaName = uri.toString(),
             mediaPath = uri.toString(),
             mediaDate = date
         )
@@ -37,26 +40,32 @@ class PhotoPickerRepositoryImpl @Inject constructor(
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun getRealPathFromUri(contentUri: Uri): String {
 
-        context.contentResolver.query(contentUri, null, null, null, null)?.use { c ->
-            val dateMod = try {
-                val colDateModified =
-                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
-                        c.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATE_TAKEN)
-                    } else {
-                        c.getColumnIndexOrThrow(DocumentsContract.Document.COLUMN_LAST_MODIFIED)
-                    }
-                c.moveToFirst()
-                c.getLong(colDateModified)
-            } catch (e: Exception) {
-                throw e
+        context.contentResolver
+            .query(contentUri, null, null, null, null)?.use { c ->
+                val dateMod = try {
+                    val colDateModified =
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            c.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATE_TAKEN)
+                        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R &&
+                            getExtensionVersion(Build.VERSION_CODES.R) >= 2
+                        ) {
+                            c.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATE_TAKEN)
+                        } else {
+                            c.getColumnIndexOrThrow(DocumentsContract.Document.COLUMN_LAST_MODIFIED)
+                        }
+                    c.moveToFirst()
+                    c.getLong(colDateModified)
+                } catch (e: Exception) {
+                    throw e
+                }
+                val result = SimpleDateFormat("yy.MM.dd", Locale.KOREA).format(dateMod)
+                Log.d(TAG, "DATE RESULT $result")
+                c.close()
+                return result
             }
-            val result = SimpleDateFormat("yy.MM.dd", Locale.KOREA).format(dateMod)
-            Log.d(TAG, "DATE RESULT $result")
-            c.close()
-            return result
-        }
 
         return getStringResource(R.string.can_not_find_date)
     }
