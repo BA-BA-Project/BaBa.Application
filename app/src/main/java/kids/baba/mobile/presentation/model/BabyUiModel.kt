8c82cab874bb0b1f4cb210e5ffdc6bf8406bb48a
TRASALBY <ghtjd8264@naver.com>
2023-04-03 20:42:36 +0900

    아이 선택 바텀시트 다이얼로그 ui 디자인

--- app/src/main/java/kids/baba/mobile/presentation/model/BabyUiModel.kt
+++ app/src/main/java/kids/baba/mobile/presentation/model/BabyUiModel.kt
@@ -3,5 +3,6 @@ package kids.baba.mobile.presentation.model
 data class BabyUiModel(
     val babyId : String,
     val groupColor: String,
-    val name: String
+    val name: String,
+    var selected: Boolean
 )
---
+++ app/src/main/java/kids/baba/mobile/presentation/view/BabyListBottomSheet.kt
@@ -0,0 +1,6 @@
+package kids.baba.mobile.presentation.view
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BabyListBottomSheet : BottomSheetDialogFragment() {
+}
