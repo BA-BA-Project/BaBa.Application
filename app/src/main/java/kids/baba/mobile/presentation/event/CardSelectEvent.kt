879c59a11fce9aae8f11c77ebe6c227224d4c642
심지훈 <simjihun@simjihuns-MacBook-Air.local>
2023-04-03 19:15:50 +0900

    Pull-Request select_card to film2

--- app/src/main/java/kids/baba/mobile/presentation/event/CardSelectEvent.kt
+++
@@ -1,7 +0,0 @@
-package kids.baba.mobile.presentation.event
-
-import kids.baba.mobile.presentation.model.CardStyleUiModel
-
-sealed class CardSelectEvent {
-    data class CardSelect(val card: CardStyleUiModel, val position: Int): CardSelectEvent()
-}
