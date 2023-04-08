82ac03872495116ff6c2e7c245c5fa919a72add1
lyh990517 <lyh990517@naver.com>
2023-04-08 21:02:28 +0900

    코드 리뷰 반영

--- app/src/main/java/kids/baba/mobile/presentation/view/MainActivity.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kids.baba.mobile.R
 import kids.baba.mobile.databinding.ActivityMainBinding
@@ -21,35 +22,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setNavController()
-        setBottomNavigation()
-    }
-
-    private fun setBottomNavigation() {
-        binding.BOTTOMMENU.run {
-            setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.collapse -> {
-                        navController.navigate(R.id.collapse_fragment)
-                        true
-                    }
-                    R.id.growth_album -> {
-                        navController.navigate(R.id.growth_album_fragment)
-                        true
-                    }
-                    R.id.my_page -> {
-                        navController.navigate(R.id.my_page_fragment)
-                        true
-                    }
-                    else -> true
-                }
-            }
-        }
     }
 
     private fun setNavController() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.CONTAINER) as NavHostFragment
         navController = navHostFragment.navController
+        binding.btvMenu.setupWithNavController(navController)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
