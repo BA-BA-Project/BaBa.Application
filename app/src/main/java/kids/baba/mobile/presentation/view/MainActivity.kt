f97436ce89dcaa90cccd3e5214b573af9bd3752e
lyh990517 <lyh990517@naver.com>
2023-04-08 20:04:13 +0900

    jetpack navigation 적용

--- app/src/main/java/kids/baba/mobile/presentation/view/MainActivity.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/MainActivity.kt
@@ -5,8 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.adapters.AdapterViewBindingAdapter.setOnItemSelectedListener
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kids.baba.mobile.R
 import kids.baba.mobile.databinding.ActivityMainBinding
@@ -14,21 +14,42 @@ import kids.baba.mobile.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setNavController()
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
         binding.BOTTOMMENU.run {
             setOnItemSelectedListener { item ->
                 when (item.itemId) {
-                    R.id.collapse -> showFragment(CollapseFragment())
-                    R.id.growth_album -> showFragment(GrowthAlbumFragment())
-                    R.id.my_page -> showFragment(MyPageFragment())
+                    R.id.collapse -> {
+                        navController.navigate(R.id.collapse_fragment)
+                        true
+                    }
+                    R.id.growth_album -> {
+                        navController.navigate(R.id.growth_album_fragment)
+                        true
+                    }
+                    R.id.my_page -> {
+                        navController.navigate(R.id.my_page_fragment)
+                        true
+                    }
                     else -> true
                 }
             }
         }
+    }
+
+    private fun setNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.CONTAINER) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -38,15 +59,6 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onKeyDown(keyCode, event)
-    }
-
-
-    private fun showFragment(fragment: Fragment): Boolean {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.CONTAINER, fragment)
-            commit()
-        }
-        return true
     }
 
     companion object {
