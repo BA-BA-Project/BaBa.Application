a025655231fa414729c500c86fbc33456130c29b
lyh990517 <lyh990517@naver.com>
2023-04-10 14:07:32 +0900

    마이페이지 유저정보 창 생성

--- app/src/main/java/kids/baba/mobile/presentation/view/MyPageFragment.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/MyPageFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import kids.baba.mobile.databinding.FragmentMypageBinding
+import kids.baba.mobile.presentation.viewmodel.MyPageViewModel
 
+@AndroidEntryPoint
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMypageBinding? = null
     private val binding
         get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
-
+    val viewModel: MyPageViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +24,10 @@ class MyPageFragment : Fragment() {
     ): View {
         _binding = FragmentMypageBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewmodel = viewModel
     }
 }
