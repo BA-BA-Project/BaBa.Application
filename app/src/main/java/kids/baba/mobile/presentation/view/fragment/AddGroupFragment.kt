5a60e36f6e088b66bf5664395b66228cc77d7de8
lyh990517 <lyh990517@naver.com>
2023-04-30 22:18:01 +0900

    그룹추가 화면 바인딩

--- app/src/main/java/kids/baba/mobile/presentation/view/fragment/AddGroupFragment.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/fragment/AddGroupFragment.kt
@@ -6,21 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kids.baba.mobile.databinding.FragmentAddGroupBinding
 import kids.baba.mobile.presentation.view.activity.MainActivity
+import kids.baba.mobile.presentation.viewmodel.AddGroupViewModel
 
 @AndroidEntryPoint
-class AddGroupFragment : Fragment(){
+class AddGroupFragment : Fragment() {
 
     private var _binding: FragmentAddGroupBinding? = null
     private val binding get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
+    private val viewModel: AddGroupViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAddGroupBinding.inflate(inflater, container, false)
+        binding.viewmodel = viewModel
         return binding.root
     }
 
@@ -41,14 +45,6 @@ class AddGroupFragment : Fragment(){
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 })
         }
-        binding.topAppBar.tvTopTitle.text = "그룹 만들기"
-        binding.bannerView.tvBannerTitle.text = "그룹을 만들어 소통을 관리해요!"
-        binding.bannerView.tvBannerDesc.text = "초대된 멤버는 자신의 그룹과\n가족 그룹의 댓글만 확인할 수 있어요"
-        binding.nameView.tvTitle.text = "그룹 이름"
-        binding.nameView.tvDesc.text = "어떤 그룹을 만들 건가요?"
-        binding.colorView.tvGroupNameTitle.text = "그룹 컬러"
-        binding.permissionView.tvTitle.text = "성장앨범 업로드 권한"
-        binding.permissionView.tvDesc.text = "없음"
     }
 
 }
