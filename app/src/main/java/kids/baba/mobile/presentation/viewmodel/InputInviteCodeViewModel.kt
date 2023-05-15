f46ed6b20d9d96d10f486323a43b7cff7c4b05b2
lyh990517 <lyh990517@naver.com>
2023-05-15 14:52:16 +0900

    초대코드로 아이추가 화면 ui모델 적용

--- app/src/main/java/kids/baba/mobile/presentation/viewmodel/InputInviteCodeViewModel.kt
+++ app/src/main/java/kids/baba/mobile/presentation/viewmodel/InputInviteCodeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kids.baba.mobile.domain.model.InviteCode
 import kids.baba.mobile.domain.usecase.AddOneBabyWithInviteCodeUseCase
+import kids.baba.mobile.presentation.model.InputInviteCodeUiModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,10 +14,7 @@ import javax.inject.Inject
 class InputInviteCodeViewModel @Inject constructor(
     private val addOneBabyWithInviteCodeUseCase: AddOneBabyWithInviteCodeUseCase
 ) : ViewModel() {
-    val title = MutableStateFlow("아이 추가하기")
-    val bannerTitle = MutableStateFlow("초대코드를\n입력해 아이를 추가해봐요.")
-    val bannerDesc = MutableStateFlow("지인의 아이를 추가하고 소통해봐요!")
-    val inputTitle = MutableStateFlow("초대코드 입력하기")
+    val uiModel = MutableStateFlow(InputInviteCodeUiModel())
 
     fun add(inviteCode: String) = viewModelScope.launch {
         addOneBabyWithInviteCodeUseCase.add(inviteCode = InviteCode(inviteCode = inviteCode))
