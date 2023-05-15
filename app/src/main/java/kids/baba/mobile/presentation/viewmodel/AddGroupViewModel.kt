f4ec9354c9ed025ca78ee620783f6cd69a5a5378
lyh990517 <lyh990517@naver.com>
2023-05-15 15:31:42 +0900

    그룹추가, 멤버 삭제 화면 ui모델 적용

--- app/src/main/java/kids/baba/mobile/presentation/viewmodel/AddGroupViewModel.kt
+++ app/src/main/java/kids/baba/mobile/presentation/viewmodel/AddGroupViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kids.baba.mobile.domain.model.MyPageGroup
 import kids.baba.mobile.domain.usecase.AddOneGroupUseCase
+import kids.baba.mobile.presentation.model.AddGroupUiModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,14 +14,7 @@ import javax.inject.Inject
 class AddGroupViewModel @Inject constructor(
     private val addOneGroupUseCase: AddOneGroupUseCase
 ) : ViewModel() {
-    val topAppBarTitle = MutableStateFlow("그룹 만들기")
-    val bannerTitle = MutableStateFlow("그룹을 만들어 소통을 관리해요!")
-    val bannerDesc = MutableStateFlow("초대된 멤버는 자신의 그룹과\n가족 그룹의 댓글만 확인할 수 있어요")
-    val nameViewTitle = MutableStateFlow("그룹 이름")
-    val nameViewDesc = MutableStateFlow("어떤 그룹을 만들 건가요?")
-    val colorViewTitle = MutableStateFlow("그룹 컬러")
-    val permissionViewTitle = MutableStateFlow("성장앨범 업로드 권한")
-    val permissionViewDesc = MutableStateFlow("없음")
+    val uiModel = MutableStateFlow(AddGroupUiModel())
 
     fun addGroup(relationGroup: String, iconColor: String) = viewModelScope.launch {
         addOneGroupUseCase.add(
