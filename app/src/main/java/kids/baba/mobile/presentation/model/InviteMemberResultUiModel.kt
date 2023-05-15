2bd9a574268819753e4f21bed68c60c1b9185750
lyh990517 <lyh990517@naver.com>
2023-05-15 16:27:00 +0900

    멤버 초대,결과,아이추가 결과 화면 Ui모델 적용

---
+++ app/src/main/java/kids/baba/mobile/presentation/model/InviteMemberResultUiModel.kt
@@ -0,0 +1,13 @@
+package kids.baba.mobile.presentation.model
+
+data class InviteMemberResultUiModel(
+    val tvTopTitleText: String = "멤버 초대",
+    val tvBannerTitleText: String = "멤버 초대를 완료했어요!",
+    val tvBannerDescText: String = "멤버들과 소통하며 즐거운 시간 보내요 :)",
+    val tvGroupTitleText: String = "초대 멤버의 소속 그룹",
+    val tvGroupDescText: String = "친구",
+    val tvRelationTitleText: String = "초대 멤버와 아이의 관계",
+    val tvRelationDescText: String = "이모",
+    val tvPermissionTitleText: String = "성장앨범 업로드 권한",
+    val tvPermissionDescText: String = "없음"
+)
