61cfadfb90750886c64fedeff81d90a6294c59cc
TRASALBY <ghtjd8264@naver.com>
2023-06-10 20:53:58 +0900

    아이 이름 변경 text입력 수정

--- app/src/main/java/kids/baba/mobile/presentation/view/bottomsheet/BabyEditProfileBottomSheet.kt
+++ app/src/main/java/kids/baba/mobile/presentation/view/bottomsheet/BabyEditProfileBottomSheet.kt
@@ -1,9 +1,13 @@
 package kids.baba.mobile.presentation.view.bottomsheet
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.annotation.StringRes
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -25,9 +29,14 @@ class BabyEditProfileBottomSheet @Inject constructor(
         get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
     private val viewModel: BabyEditProfileBottomSheetViewModel by viewModels()
 
+
+    private lateinit var imm: InputMethodManager
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindViewModel()
+        setInputManager()
+        setEditTextFocusEvent()
         collectEvent()
     }
 
@@ -35,10 +44,14 @@ class BabyEditProfileBottomSheet @Inject constructor(
         viewLifecycleOwner.repeatOnStarted {
             viewModel.eventFlow.collect { event ->
                 when (event) {
-                    is BabyEditEvent.SuccessBabyEdit -> {
-                        itemClick(event.babyName)
-                        dismiss()
+                    is BabyEditEvent.NameInput -> {
+                        keyboardShow(binding.nameView.tvEdit)
                     }
+
+                    is BabyEditEvent.SuccessBabyEdit -> {
+                        changeBabyName(event.babyName)
+                    }
+
                     is BabyEditEvent.ShowSnackBar -> {
                         dismiss()
                         showSnackBar(event.message)
@@ -60,6 +73,41 @@ class BabyEditProfileBottomSheet @Inject constructor(
     private fun bindViewModel() {
         binding.viewmodel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun setInputManager() {
+        imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    private fun setEditTextFocusEvent() {
+        binding.nameView.tvEdit.setOnFocusChangeListener { _, hasFocus ->
+            viewModel.changeNameFocus(hasFocus)
+        }
+        binding.nameView.tvEdit.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                keyboardDown(binding.nameView.tvEdit)
+                changeBabyName(binding.nameView.tvEdit.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun keyboardDown(editText: EditText) {
+        editText.clearFocus()
+        imm.hideSoftInputFromWindow(editText.windowToken, 0)
+    }
+
+
+    private fun keyboardShow(editText: EditText) {
+        editText.requestFocus()
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun changeBabyName(name: String) {
+        itemClick(name)
+        dismiss()
     }
 
 
